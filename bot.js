79066a5c58688b1e75da7c4d16cc9fc95efba291
Kevin Thomas <kevin41212@gmail.com>
2025-09-12 12:25:40 -0400

    adding slash commands

--- bot.js
+++ bot.js
@@ -71,3 +71,15 @@ client.once('ready', async () => {
 });
 
 client.login(process.env.TOKEN);
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
